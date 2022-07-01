6066a74f93a91af21e57971d2b38132bab486046
HR055 <108529274+HR055@users.noreply.github.com>
2022-07-01 14:44:21 +0200

    Add files via upload

---
+++ temp&humidity/config.py
@@ -0,0 +1,9 @@
+# Wifi credentials
+SSID = ''
+PASS = ''
+
+
+# Datacake credentials
+SERIAL_NUMBER = '1234Hayden1234'
+MQTT_BROKER = 'mqtt.datacake.co'
+TOKEN = 'c55a9fa7d30fdca88b726a0cce7e33fec0f9541c'
